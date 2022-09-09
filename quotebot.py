5f17c99adae5be91ba18854c1cfb3b02826afa84
Anthony Kabanets <54618467+TheWizland@users.noreply.github.com>
2022-09-09 13:55:35 -0700

    Add files via upload
    - Bot now uses intents. Requires privileged intent for message_content.
    - Split quote command into quote and addQuote.
    quote [name] [numQuotes]
    	Fetches and prints quotes.
    	numQuotes specifies how many quotes to print. Min is 1, max is 20. Can be left blank, default is 1.
    addQuote [name] [text]
    	Saves a quote to the database.
    	Attachments (smaller than 8 MB) are allowed.

--- quotebot.py
+++ quotebot.py
@@ -5,6 +5,7 @@ import logging
 import sqlite3
 import os
 import datetime
+import time
 from discord.reaction import Reaction
 
 #ruamel is just a nicer json tbh
@@ -13,20 +14,20 @@ from discord.reaction import Reaction
 from ruamel.yaml import YAML
 yaml = YAML()
 
-with open("./config.yml", "r", encoding = "utf-8") as file: #utf-8 as standard
+with open("config.yaml", "r", encoding = "utf-8") as file: #utf-8 as standard
     config = yaml.load(file)
 
 logging.basicConfig(level=logging.INFO)
 
-bot = commands.Bot(command_prefix=config["Prefix"])
-#prefix is $
+botIntents = discord.Intents.default()
+botIntents.message_content = True
+bot = commands.Bot(command_prefix=config["Prefix"], intents = botIntents)
 emoji = '✅'
 
 con = sqlite3.connect(config['Quotes'])
 cur = con.cursor()
 
 cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='quotes' ''')
-lastId = None
 
 if(cur.fetchone()[0] == 0) : {
     cur.execute('''CREATE TABLE quotes 
@@ -43,8 +44,6 @@ async def printQuote(ctx, output): #output comes from cur.fetchone()
         await ctx.channel.send("No valid quotes found.")
     else:
         outputString = content=str(output[1] or '') + '\n-' + output[2] + ', ' + output[4] + ", ID: " + str(output[0])
-        global lastId 
-        lastId = output[0]
         if(output[5]): #output[5] is file extension column.
             file = discord.File(config["Attachments"] + str(output[0]) + '.' + output[5])
             await ctx.channel.send(file = file, content=outputString)
@@ -102,43 +101,54 @@ async def idQuote(ctx, id):
     #await ctx.message.add_reaction(emoji)
 
 @bot.command()
-async def quote(ctx, quoteAuthor, *, quote = None):
-    if(quote is None and not ctx.message.attachments):
-        cur.execute("SELECT COUNT() FROM quotes WHERE quoteAuthor = :name", {"name": quoteAuthor})
-        numQuotes = cur.fetchone()[0]
-        if(lastId and numQuotes > 1): #Exlude most recently quoted message, to prevent repeat quotes. lastId is a global variable and is assigned in printQuote()
-            cur.execute("SELECT * FROM quotes WHERE quoteAuthor = :name AND NOT id = :lastId ORDER BY RANDOM() LIMIT 1", {"name": quoteAuthor, "lastId": lastId})
-        else:
-            cur.execute("SELECT * FROM quotes WHERE quoteAuthor = :name ORDER BY RANDOM() LIMIT 1", {"name": quoteAuthor})
-        
-        output = cur.fetchone()
-        await printQuote(ctx, output)
+async def addQuote(ctx, quoteAuthor, *, quote = None):
+    date = datetime.date.today()
+    if ctx.message.attachments:
+        if(ctx.message.attachments[0].size > 8000000): #Capped at 8 MB. Bot cannot send files larger than 8 MB.
+            await ctx.message.send("This file is too large.")
+            return
+
+        fileExtension = ctx.message.attachments[0].filename #Probably a better way to do this, but I don't know how.
+        fileExtension = fileExtension.rsplit('.', 1)[-1] #All text after last dot. If filename has no dot, entire filename will be saved. This is bad.
+    elif quote:
+        fileExtension = None
     else:
-        date = datetime.date.today()
-        if(ctx.message.attachments):
-            if(ctx.message.attachments[0].size > 8000000): #Capped at 8 MB. Bot cannot send files larger than 8 MB.
-                await ctx.message.send("This file is too large.")
-                return
-            fileExtension = ctx.message.attachments[0].filename
-            fileExtension = fileExtension.rsplit('.', 1)[-1] #All text after last dot. If filename has no dot, entire filename will be saved. This is bad.
-        else:
-           fileExtension = None
-        
-        cur.execute("INSERT INTO quotes(quote, quoteAuthor, quoteRecorder, date, fileExtension) VALUES (?, ?, ?, ?, ?)", (quote, quoteAuthor, ctx.author.name, date, fileExtension))
+        await ctx.channel.send("No quote provided.")
+        return
+    
+    cur.execute("INSERT INTO quotes(quote, quoteAuthor, quoteRecorder, date, fileExtension) VALUES (?, ?, ?, ?, ?)", (quote, quoteAuthor, ctx.author.name, date, fileExtension))
 
-        if(ctx.message.attachments):
-            await ctx.message.attachments[0].save(config["Attachments"] + str(cur.lastrowid) + '.' + fileExtension)
-            #Attachment filename is based on unique id of the quote.
-            #Saved files will never have the same filename.
-            #Only one attachment can be saved per quote.
+    if(ctx.message.attachments): #Save message attachment.
+        await ctx.message.attachments[0].save(config["Attachments"] + str(cur.lastrowid) + '.' + fileExtension)
+        #Attachment filename is based on unique id of the quote.
+        #Saved files will never have the same filename.
+        #Only one attachment can be saved per quote.
 
-        con.commit()
-        await ctx.channel.send("Quote #" + str(cur.lastrowid) + " saved.")
-
+    con.commit()
+    await ctx.channel.send("Quote #" + str(cur.lastrowid) + " saved.")
     await ctx.message.add_reaction(emoji)
 
 @bot.command()
-@commands.has_role(config["PermRole"])
+async def quote(ctx, quoteAuthor, numQuotes = 1):
+    #cur.execute("SELECT COUNT() FROM quotes WHERE quoteAuthor = :name", {"name": quoteAuthor})
+    if(numQuotes < 1):
+        numQuotes = 1
+    if(numQuotes > 20):
+        numQuotes = 20 #Min is 1, max is 20.
+    
+    cur.execute("SELECT * FROM quotes WHERE quoteAuthor = :name ORDER BY RANDOM() LIMIT :numQuotes", {"name": quoteAuthor, "numQuotes": numQuotes})
+    output = cur.fetchall()
+
+    if(output):
+        for quote in output:
+            await printQuote(ctx, quote)
+            time.sleep(0.3)
+    else:
+        await ctx.channel.send("No quotes found.")
+    await ctx.message.add_reaction(emoji)
+
+@bot.command()
+@commands.has_role(config["Permissions Role"])
 async def deleteQuote (ctx, id):
     await ctx.channel.send("Deleting quote...")
     await idQuote(ctx, id)
@@ -159,16 +169,20 @@ async def deleteQuote (ctx, id):
 async def on_command_error(ctx, error):
     if(isinstance(error, commands.MissingRole)):
         await ctx.send("Required role missing.")
+    elif(isinstance(error, commands.CommandNotFound)):
+        await ctx.send("Command not found.")
 
 #restart the bot
 @bot.command(name ="restart", aliases = ["r"], help = "Restarts the bot.")
+@commands.has_role(config["Permissions Role"])
 async def restart(ctx): #ctx passes an argument into the body. a "context" (ctx)
     #sends react to message as confirmation to restart
     await ctx.message.add_reaction(emoji)
     con.close()
     await bot.close()
+    
 
-bot.run(config["Token"], bot=True, reconnect=True)
+bot.run(config["Token"], reconnect=True)
 #end command lets the client know that it is a bot
 #also if connection drops, bot will attempt to reconnect
 #saves trouble of manually restarting in the event of connection loss
