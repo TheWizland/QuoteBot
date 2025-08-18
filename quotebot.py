c4b75cd190e0aa32778653bfaf79393dda4355a3
Anthk <tkabanets@hotmail.com>
2025-08-17 23:01:56 -0700

    Fixed file size too large warning.

--- quotebot.py
+++ quotebot.py
@@ -84,8 +84,10 @@ async def addQuote(ctx, quoteAuthor, *, quote = None):
         Printer(e)
     
     if ctx.message.attachments:
-        if(ctx.message.attachments[0].size > constants.MAX_FILESIZE): #Capped at 8 MB. Bot cannot send files larger than 8 MB.
-            await ctx.message.send("This file is too large.")
+        if(ctx.message.attachments[0].size > constants.MAX_FILESIZE):
+            receivedSize = str(ctx.message.attachments[0].size/1000000)
+            maxSize = str(constants.MAX_FILESIZE/1000000)
+            await ctx.channel.send("This file is too large. (Received Size: " + receivedSize + " MB, Max Size: " + maxSize + " MB)")
             return
 
         fileExtension = ctx.message.attachments[0].filename #Probably a better way to do this, but I don't know how.
@@ -113,6 +115,7 @@ async def addQuote(ctx, quoteAuthor, *, quote = None):
 async def refreshCommands(ctx):
     for extension in extensions:
         await bot.reload_extension(extension)
+    await bot.tree.sync(guild=discord.Object(id=902040464893554698))
     await ctx.send("Commands synced.")
 
 bot.run(config["Token"], reconnect=True)
